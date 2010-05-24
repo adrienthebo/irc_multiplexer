df6520e9c9861091bb76913e07931fa0bea60746
Adrien Thebo <adrien.thebo@gmail.com>
2010-05-24 15:00:59 -0700

    Added better reading/responding to client.c

--- client.c
+++ client.c
@@ -41,11 +41,33 @@ int main(int argc, char **argv) {
     buf[rcvbuf + 1] = '\0';
 
     while(1) {
+	memset(buf, 0, rcvbuf + 1);
 	ssize_t retrieved = recv(fd, buf, rcvbuf, 0);
-	if(retrieved > rcvbuf) {
+	signed int so_error = 0;
+	unsigned int so_error_len = sizeof(so_error);
+
+	getsockopt(fd, SOL_SOCKET, SO_ERROR, &so_error, &so_error_len);
+
+	if(so_error != 0) {
+	    fprintf(stdout, "Socket connection closed\n");
+	    exit(1);
+	}
+	
+	else if(retrieved > rcvbuf) {
 	    fprintf(stderr, "ERROR: Data truncation occurred!\n");
 	}
 
-	fputs(buf, stdout);
+	fprintf(stdout, "Message received: \n\"%s\"\n---\n", buf);
+
+	memset(buf, 0, rcvbuf + 1);
+	strcpy(buf, "foo\r\n");
+
+	ssize_t sent = send(fd, buf, strlen(buf), 0);
+
+	if(sent != strlen(buf)) {
+	    fprintf(stderr, "ERROR: dur wut\n");
+	}
     }
+    fprintf(stdout, "Exiting.\n");
+    return 0;
 }
